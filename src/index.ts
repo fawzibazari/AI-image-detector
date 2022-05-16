a90099ac2d2d8f21c1d7dcef771eb4abf19f75ee
fawzibazari <fawzi.bazari2@gmail.com>
2022-05-16 16:07:13 +0200

    added parcel and setup tenserflow js

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { config as dotenvConfig } from 'dotenv';
-dotenvConfig();
+import tf from '@tensorflow/tfjs';
+import mobilenetModule from '@tensorflow-models/mobilenet';
+import knnClassifier from '@tensorflow-models/knn-classifier';
 
-console.log('running');
+// Create the classifier.
+const classifier = knnClassifier.create();
+
+console.log('haha');
