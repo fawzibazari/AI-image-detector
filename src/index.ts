ed4a0100a7c74b9090d4bec820a784269b8b601a
fawzibazari <fawzi.bazari2@gmail.com>
2022-05-17 10:26:51 +0200

    basically it is working

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,56 @@
-import tf from '@tensorflow/tfjs';
-import mobilenetModule from '@tensorflow-models/mobilenet';
-import knnClassifier from '@tensorflow-models/knn-classifier';
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+const webcamElement = document.getElementById('webcam');
+
+let net: any;
 
 // Create the classifier.
+// Knn = key nearest neighbors
 const classifier = knnClassifier.create();
 
-console.log('haha');
+async function main() {
+  net = await mobilenet.load();
+
+  console.log('here');
+
+  const webcam = await tf.data.webcam(webcamElement as HTMLVideoElement);
+
+  const addImage = async (imageId: number) => {
+    const img = await webcam.capture();
+    const activation = net.infer(img, true);
+    classifier.addExample(activation, imageId);
+    img.dispose();
+  };
+
+  document
+    .getElementById('fawzi')
+    ?.addEventListener('click', () => addImage(0));
+  document
+    .getElementById('kharty')
+    ?.addEventListener('click', () => addImage(1));
+  document
+    .getElementById('kharty et fawzi')
+    ?.addEventListener('click', () => addImage(2));
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      const img = await webcam.capture();
+      // 'conv_preds' is the logits activation of MobileNet.
+      // damn it took me some time to find out what 'conv_preds' meant
+      const activation = net.infer(img, 'conv_preds');
+      const result = await classifier.predictClass(activation);
+      console.log(result);
+
+      const Images = ['Fawzi', 'Kharty', 'Kharty et Fawzi'];
+      document.getElementById('div')!.innerText = `prediction : ${result.label}
+      probabilty: ${result.confidences[result.label]}
+      `;
+
+      img.dispose();
+    }
+    await tf.nextFrame();
+  }
+}
+
+main();
